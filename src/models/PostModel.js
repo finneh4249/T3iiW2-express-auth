d176855b6a0e2a8c4293fbc117532364f67ae0cf
Ethan Cornwill <ethancornwill@outlook.com>
2025-01-21 20:23:11 +1100

    Add Post and User models with Mongoose schemas

---
+++ src/models/PostModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    }
+})
+
+const PostModel = mongoose.model('Post', PostSchema);
+
+module.exports = PostModel;
---
+++ src/models/UserModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 3,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    }
+})
+
+const UserModel = mongoose.model('User', UserSchema);
+
+module.exports = UserModel;
